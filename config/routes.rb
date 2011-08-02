470f6e35f7ee151efd550bbed21cf8637d773bd8
anupriya <anu.d@railsbuddies.com>
2011-08-02 10:54:37 +0530

    routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 	match '/admin/store_details'=>'admin/store_details#index'
 	match '/admin/store_details/billing_history'=>'admin/store_details#billing_history'
 	match '/admin/store_details/store_billing'=>'admin/store_details#store_billing'
-	match '/admin/owners'=>'admin/users#index',:as =>:admin_store_owners
-	match 'store/:id' => 'stores_registration#new_store'
+	#~ match '/admin/owners'=>'admin/users#index',:as =>:admin_store_owners
+  match '/admin/users/store_owners'=>'admin/users#store_owners',:as =>:store_owners
+  match 'store/:id' => 'stores_registration#new_store'
 	match 'store' => 'stores_registration#save_store_details'
 	match '/storeowner' => 'stores_registration#storeowner',:as=>:storeowner
 	match 'user_update_plan/:plan_id'=>'admin/upgrade_plans#user_update_plan',:as=>:update_plan
@@ -15,5 +16,5 @@ Rails.application.routes.draw do
 		resources :upgrade_plans
 	end
 	resources :stores_registration
- 
+
 end
