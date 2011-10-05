7dd8c96d71c626e476170c6dfbe5c275fc160926
Dhibika <dhibika.baskaran@sedin.co.in>
2011-10-05 15:34:16 +0530

    changes for syntax changes in spree version

--- app/models/promotion_decorator.rb
+++ app/models/promotion_decorator.rb
@@ -1,10 +1,10 @@
 Promotion.class_eval do
 #Overriding eligible? method to identify the domain
- def eligible?(order, options = {})
+ def eligible?(order)
 	if order
 	 store=StoreownerOrder.find_by_order_id(order.id).store_owner
     if self.domain_url == store.domain
-    !expired? && rules_are_eligible?(order, options) && coupon_is_eligible?(order, options[:coupon_code])
+    !expired? && rules_are_eligible?(order)
 		else
 			return false
   end
