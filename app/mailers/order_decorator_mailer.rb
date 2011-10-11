4800b6c2f38350ed7d9c2c59c59caec836e8698b
Anupriya <anu.d@railsfactory.org>
2011-10-11 12:28:40 +0530

    indentation changes for mailer

--- app/mailers/order_decorator_mailer.rb
+++ app/mailers/order_decorator_mailer.rb
@@ -6,30 +6,29 @@ class OrderMailer < ActionMailer::Base
     @order = order
     @domain=find_mail_domain(order)
     unless @domain.blank?
-    subject = (resend ? "[RESEND] " : "")
-    available_mail=mail_settings(@domain)
-    subject += "#{Spree::Config[:site_name]} #{t('subject', :scope =>'order_mailer.confirm_email')} ##{order.number}"
-    if available_mail
-    mail(:to => order.email,:subject => subject)
-    else
-    mail(:from =>find_storeowner_email(order) ,:to => order.email,:subject => subject)
-   end
-   end
+      subject = (resend ? "[RESEND] " : "")
+      available_mail=mail_settings(@domain)
+      subject += "#{Spree::Config[:site_name]} #{t('subject', :scope =>'order_mailer.confirm_email')} ##{order.number}"
+      if available_mail
+        mail(:to => order.email,:subject => subject)
+      else
+        mail(:from =>find_storeowner_email(order) ,:to => order.email,:subject => subject)
+      end
+    end
   end
 
   def cancel_email(order, resend=false)
-@order = order
-@domain=find_mail_domain(order)
-  unless @domain.blank?
-  mail_settings(@domain)
-  subject = (resend ? "[RESEND] " : "")
-  subject += "#{Spree::Config[:site_name]} #{t('subject', :scope => 'order_mailer.cancel_email')} ##{order.number}"
-    if available_mail
-    mail(:to => order.email,:subject => subject)
-    else
-    mail(:from =>find_storeowner_email(order),:to => order.email,:subject => subject)
-
+    @order = order
+    @domain=find_mail_domain(order)
+    unless @domain.blank?
+      mail_settings(@domain)
+      subject = (resend ? "[RESEND] " : "")
+      subject += "#{Spree::Config[:site_name]} #{t('subject', :scope => 'order_mailer.cancel_email')} ##{order.number}"
+      if available_mail
+        mail(:to => order.email,:subject => subject)
+      else
+        mail(:from =>find_storeowner_email(order),:to => order.email,:subject => subject)
+      end
     end
   end
-end
 end
--- app/mailers/shipment_decorator_mailer.rb
+++ app/mailers/shipment_decorator_mailer.rb
@@ -2,21 +2,18 @@ class ShipmentMailer < ActionMailer::Base
   helper "spree/base"
   require "mail_domain_interceptor"
   include Spree::BaseHelper
-def shipped_email(shipment, resend=false)
+  def shipped_email(shipment, resend=false)
     @domain=find_mail_domain(shipment.order)
-unless @domain.blank?
-    available_mail=mail_settings(@domain)
-
-    @shipment = shipment
-    subject = (resend ? "[RESEND] " : "")
-    subject += "#{Spree::Config[:site_name]} #{t('subject', :scope => 'shipment_mailer.shipped_email')} ##{shipment.order.number}"
-        if available_mail
-
-    mail(:to => shipment.order.email,:subject => subject)
-         else
-              mail(:from =>find_storeowner_email(shipment.order),:to => shipment.order.email,:subject => subject)
-
-       end
-       end
+    unless @domain.blank?
+      available_mail=mail_settings(@domain)
+      @shipment = shipment
+      subject = (resend ? "[RESEND] " : "")
+      subject += "#{Spree::Config[:site_name]} #{t('subject', :scope => 'shipment_mailer.shipped_email')} ##{shipment.order.number}"
+      if available_mail
+          mail(:to => shipment.order.email,:subject => subject)
+      else
+          mail(:from =>find_storeowner_email(shipment.order),:to => shipment.order.email,:subject => subject)
+      end
     end
   end
+end
