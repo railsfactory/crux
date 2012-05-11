d3595e08ed586d0f483d83aa1f784cdccaa6b5cc
Dhibika <dhibika.baskaran@sedin.co.in>
2012-05-11 11:24:12 +0530

    changes for product display

--- app/controllers/products_decorator_controller.rb
+++ app/controllers/products_decorator_controller.rb
@@ -4,6 +4,8 @@ include Admin::BaseHelper
     @searcher = Spree::Config.searcher_class.new(params)
     domain = get_sub_domain(current_subdomain)
     @products = @searcher.retrieve_products(domain)
+			p=Product.find(:all,:conditions=>["domain_url=?",get_sub_domain(current_subdomain)],:order=>"id desc",:limit=>10).map(&:id)
+	   @products=@products.reject{|x| p.include?(x.id)}
     respond_with(@products)
   end
 end
