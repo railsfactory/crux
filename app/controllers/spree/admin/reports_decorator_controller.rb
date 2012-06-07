2eb2d9296612df4e35aac8abcc83fdf63e5ff089
railsfactory-nitin <nitin@railsfactory.org>
2012-06-07 16:49:59 +0530

    delete_cart

--- app/controllers/spree/admin/reports_decorator_controller.rb
+++ app/controllers/spree/admin/reports_decorator_controller.rb
@@ -1,34 +1,53 @@
-Spree::Admin::ReportsController.class_eval do
- def sales_total
-    params[:search] = {} unless params[:search]
-    if params[:search][:created_at_greater_than].blank?
-      params[:search][:created_at_greater_than] = Time.zone.now.beginning_of_month
-    else
-      params[:search][:created_at_greater_than] = Time.zone.parse(params[:search][:created_at_greater_than]).beginning_of_day rescue Time.zone.now.beginning_of_month
-    end
-    if params[:search] && !params[:search][:created_at_less_than].blank?
-      params[:search][:created_at_less_than] = Time.zone.parse(params[:search][:created_at_less_than]).end_of_day rescue ""
-    end
-    if params[:search].delete(:completed_at_is_not_null) == "1"
-      params[:search][:completed_at_is_not_null] = true
-    else
-      params[:search][:completed_at_is_not_null] = false
-    end
-    params[:search][:meta_sort] ||= "created_at.desc"
-    @search = Spree::Order.search(params[:search])
-    @orders = @search.result(:distinct => true)
-    unless current_user.has_role?"admin"
-      store=current_user.store_owner
-      orders=store.storeowner_orders.map(&:order_id)
-      @item_total = @search.where("id in (?)",orders).sum(:item_total)
-      @adjustment_total = @search.where("id in (?)",orders).sum(:adjustment_total)
-      puts @search.where("id in (?)",orders).map(&:adjustment_total)
-      puts @search.where("id in (?)",orders).map(&:created_at)
-      @sales_total = @search.where("id in (?)",orders).sum(:total)
-    else
-      @item_total = @search.sum(:item_total)
-      @adjustment_total = @search.sum(:adjustment_total)
-      @sales_total = @search.sum(:total)
+module Spree
+  module Admin
+    class ReportsController < Spree::Admin::BaseController
+      before_filter :load_data
+      respond_to :html
+
+      AVAILABLE_REPORTS = {
+        :sales_total => {:name => I18n.t(:sales_total), :description => I18n.t(:sales_total_description)}
+      }
+
+      def index
+        @reports = AVAILABLE_REPORTS
+        respond_with(@reports)
+      end
+
+      def sales_total
+        params[:q] = {} unless params[:q]
+
+        if params[:q][:created_at_gt].blank?
+          params[:q][:created_at_gt] = Time.zone.now.beginning_of_month
+        else
+          params[:q][:created_at_gt] = Time.zone.parse(params[:q][:created_at_gt]).beginning_of_day rescue Time.zone.now.beginning_of_month
+        end
+
+        if params[:q] && !params[:q][:created_at_lt].blank?
+          params[:q][:created_at_lt] = Time.zone.parse(params[:q][:created_at_lt]).end_of_day rescue ""
+        end
+
+        if params[:q].delete(:completed_at_not_null) == "1"
+          params[:q][:completed_at_not_null] = true
+        else
+          params[:q][:completed_at_not_null] = false
+        end
+
+        params[:q][:s] ||= "created_at desc"
+
+        @search = Order.complete.ransack(params[:q])
+        @orders = @search.result
+        @item_total = @orders.sum(:item_total)
+        @adjustment_total = @orders.sum(:adjustment_total)
+        @sales_total = @orders.sum(:total)
+
+        respond_with
+      end
+
+      private
+      def load_data
+
+      end
+
     end
   end
 end
