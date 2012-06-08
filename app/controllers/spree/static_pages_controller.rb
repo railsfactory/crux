40207525e9150f209f67e2ec0121ae05e360d0d0
railsfactory-nitin <nitin@railsfactory.org>
2012-06-08 13:05:34 +0530

    static_files

---
+++ app/controllers/spree/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class Spree::StaticPagesController < Spree::BaseController
+    layout 'spree/layouts/static'
+  def aboutus
+	
+  end
+
+  def contactus
+	 
+  end
+
+  def privacy
+	  
+  end
+
+  def terms
+	  
+  end
+	
+end
