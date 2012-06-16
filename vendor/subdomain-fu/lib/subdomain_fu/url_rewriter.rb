9da886cfe8f558cdfb80eee37201d5bc59b6ad66
Ramkumar <ramkumar@railsfactory.org>
2012-06-16 19:13:12 +0530

    vendor subdomain added

---
+++ vendor/subdomain-fu/lib/subdomain_fu/url_rewriter.rb
@@ -0,0 +1,19 @@
+require 'action_dispatch/routing/route_set'
+
+module ActionDispatch
+  module Routing
+    class RouteSet #:nodoc:
+      def url_for_with_subdomains(options, path_segments=nil)
+        if SubdomainFu.needs_rewrite?(options[:subdomain], (options[:host])) || options[:only_path] == false
+          options[:only_path] = false if SubdomainFu.override_only_path?
+          options[:host] = SubdomainFu.rewrite_host_for_subdomains(options.delete(:subdomain), options[:host])
+          # puts "options[:host]: #{options[:host].inspect}"
+        else
+          options.delete(:subdomain)
+        end
+        url_for_without_subdomains(options)
+      end
+      alias_method_chain :url_for, :subdomains
+    end
+  end
+end
